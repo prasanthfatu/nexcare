763e009bd3747bd3e06ab4298ba384a980ee48c1
Prasanth R <prasanthraja58@gmail.com>
2025-07-10 20:06:01 +0530

    modified page

--- src/features/auth/About.js
+++ src/features/auth/About.js
@@ -2,30 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
-
+import useAuth from '../../hooks/useAuth'
 
 const About = () => {
 
+  const { dark } = useAuth()
+
   return (
-    <section className='about'>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent:'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.7)', width: '100vw', height: '100vh'}}>
 
-      <Link to='/'><FontAwesomeIcon icon={faArrowLeft} /></Link>
+      <section className='about'>
 
-      <div className="about-container">
+        <Link to='/' style={{backgroundColor: 'blue'}}><FontAwesomeIcon icon={faArrowLeft} style={{color: dark ? "black" : 'whitesmoke'}} /></Link>
 
-        <h1 className='about-head'>About Us</h1>
+        <div className="about-container" style={{backgroundColor: dark ? '#121212' : 'whitesmoke'}}>
 
-        <p className='about-para'>At NexCare Innovate, we are committed to providing high-quality healthcare services. Our team of skilled professionals is dedicated to ensuring you receive the best possible care in a compassionate and welcoming environment.</p>
+          <h1 className='about-head'>About Us</h1>
 
-        <p className='about-para'>The establishment of a comprehensive medical test center represents a significant opportunity to address the growing demand for high-quality diagnostic testing services in our community. By offering a wide range of tests, advanced technology, and patient-centered care, we aim to make a positive impact on the health and well-being of individuals and contribute to the advancement of healthcare delivery.</p>
+          <p className='about-para'>At NexCare Innovate, we are committed to providing high-quality healthcare services. Our team of skilled professionals is dedicated to ensuring you receive the best possible care in a compassionate and welcoming environment.</p>
 
-        <p className='about-para'>
-          At Nexcare Innovate, we are dedicated to providing comprehensive medical testing services to meet the diverse healthcare needs of our patients. With state-of-the-art technology, experienced medical professionals, and a commitment to excellence, we strive to deliver accurate, reliable, and timely test results to support informed decision-making and promote overall well-being.
-        </p>
+          <p className='about-para'>The establishment of a comprehensive medical test center represents a significant opportunity to address the growing demand for high-quality diagnostic testing services in our community. By offering a wide range of tests, advanced technology, and patient-centered care, we aim to make a positive impact on the health and well-being of individuals and contribute to the advancement of healthcare delivery.</p>
 
-      </div>
-      
-    </section>
+          <p className='about-para lastpara'>
+            At Nexcare Innovate, we are dedicated to providing comprehensive medical testing services to meet the diverse healthcare needs of our patients. With state-of-the-art technology, experienced medical professionals, and a commitment to excellence, we strive to deliver accurate, reliable, and timely test results to support informed decision-making and promote overall well-being.
+          </p>
+
+        </div>
+        
+      </section>
+    </div>
   )
 }
 
